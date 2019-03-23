ff3589b2cf413a5c53160eb273052c238fad68bf
TheBlocks <21197843+TheBlocks@users.noreply.github.com>
2019-03-24 00:43:38 +0100

    Add starting files

---
+++ anarchy/anarchy.py
@@ -0,0 +1,14 @@
+from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
+from rlbot.utils.structures.game_data_struct import GameTickPacket
+
+
+class Anarchy(BaseAgent):
+    def __init__(self, name, team, index):
+        super().__init__(name, team, index)
+        pass
+
+    def initialize_agent(self):
+        pass
+
+    def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
+        return SimpleControllerState()
