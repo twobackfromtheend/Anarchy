0101c3ae782186772d009b0a53d6dbb94aa4cd93
Robbie <42142350+robbai@users.noreply.github.com>
2019-03-24 11:54:18 +0000

    F L O A T (#23)
    * Add a controller
    
    * Revert "Add a controller"
    
    This reverts commit 363fbc221e40cc4fde7fb60be04b534c9466ebdf.
    
    * Revert "Revert "Add a controller""
    
    This reverts commit b0521e28565fd96d786f173db44a97221a35cdf2.
    
    * Never leave the controller behind!
    
    * Sign
    
    * Use sign!
    
    * Vectors class
    
    * Update anarchy.py
    
    * F L O A T

--- anarchy/utils.py
+++ anarchy/utils.py
@@ -1,11 +1,11 @@
 def sign(value: float) -> float:
     return 0 if value == 0 else (1 if value > 0 else - 1)
 
-def clamp(x, min_, max_) -> float:
-    return max(min(x,max_),min_)
+def clamp(x: float, min_: float, max_: float) -> float:
+    return max(min(x, max_), min_)
 
-def clamp01(x) -> float:
+def clamp01(x: float) -> float:
     return clamp(x, 0, 1)
 
-def clamp11(x) -> float:
+def clamp11(x: float) -> float:
     return clamp(x, -1, 1)
