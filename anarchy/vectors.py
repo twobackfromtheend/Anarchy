8995e8851288164fc9d425e2eb41a44ade71bb90
Robbie <42142350+robbai@users.noreply.github.com>
2019-03-24 11:58:50 +0000

    Vector3 (#24)
    * Add a controller
    
    * Revert "Add a controller"
    
    This reverts commit 363fbc221e40cc4fde7fb60be04b534c9466ebdf.
    
    * Revert "Revert "Add a controller""
    
    This reverts commit b0521e28565fd96d786f173db44a97221a35cdf2.
    
    * Never leave the controller behind!
    
    * Sign
    
    * Use sign!
    
    * Vectors class
    
    * Update anarchy.py
    
    * F L O A T
    
    * Vector3

--- anarchy/vectors.py
+++ anarchy/vectors.py
@@ -16,3 +16,15 @@ class Vector2:
     def correction_to(self, ideal):
         correction = math.atan2(self.y, -self.x) - math.atan2(ideal.y, -ideal.x) # The in-game axes are left handed, so use -x
         return correction if abs(correction) <= math.pi else (sign(correction) * 2 * math.pi) # Make sure we go the 'short way'
+
+class Vector3:
+    def __init__(self, x=0, y=0, z=0):
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
+
+    def __add__(self, val):
+        return Vector3(self.x + val.x, self.y + val.y, self.z + val.z)
+
+    def __sub__(self, val):
+        return Vector3(self.x - val.x, self.y - val.y, self.z - val.z)
